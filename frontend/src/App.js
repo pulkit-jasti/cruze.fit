6a8437fc0f915ab50b675e07490efe25fac570c8
pulkit-jasti <pulkit.jasti@gmail.com>
2022-03-26 23:19:32 -0700

    Styling update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
+import React, { Component } from "react";
+
 import ExerciseCard from "./components/ExerciseCard";
 import "./style.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import pushup from "./assets/pushup.png";
 import skipping from "./assets/skipping.png";
@@ -8,19 +11,46 @@ import pullup from "./assets/pullup.png";
 import weight from "./assets/weight.png";
 import situps from "./assets/situps.png";
 
-function App() {
-  return (
-    <main>
-      <div className="card-container">
-        <ExerciseCard img={pushup} title="Pushups" />
-        <ExerciseCard img={skipping} title="Skipping" />
-        <ExerciseCard img={lunges} title="Lunges" />
-        <ExerciseCard img={pullup} title="Pullup" />
-        <ExerciseCard img={weight} title="Weight Lifting" />
-        <ExerciseCard img={situps} title="Situps" />
-      </div>
-    </main>
-  );
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  }
+
+  render() {
+    return (
+      <main>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} centered>
+          {/* <ModalHeader toggle={this.toggle}>Modal title</ModalHeader> */}
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </ModalBody>
+        </Modal>
+
+        <div className="card-container">
+          <ExerciseCard img={pushup} title="Pushups" onClick={this.toggle} />
+          <ExerciseCard img={skipping} title="Skipping" />
+          <ExerciseCard img={lunges} title="Lunges" />
+          <ExerciseCard img={pullup} title="Pullup" />
+          <ExerciseCard img={weight} title="Weight Lifting" />
+          <ExerciseCard img={situps} title="Situps" />
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
--- frontend/src/components/ExerciseCard.jsx
+++ frontend/src/components/ExerciseCard.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class ExerciseCard extends Component {
   render() {
     return (
-      <div className="exercise-card">
+      <div className="exercise-card" onClick={this.props.onClick}>
         <img src={this.props.img} alt="" />
         <h3>{this.props.title}</h3>
       </div>
