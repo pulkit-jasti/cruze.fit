5e57ce157bd2cb60226fdde2d174b6554eddd150
pulkit-jasti <pulkit.jasti@gmail.com>
2022-03-26 20:43:25 -0700

    Card updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import ExerciseCard from "./components/ExerciseCard";
 import "./style.scss";
+
+import pushup from "./assets/pushup.png";
+import skipping from "./assets/skipping.png";
+import lunges from "./assets/lunges.png";
+import pullup from "./assets/pullup.png";
+import weight from "./assets/weight.png";
+import situps from "./assets/situps.png";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <main>
+      <div className="card-container">
+        <ExerciseCard img={pushup} title="Pushups" />
+        <ExerciseCard img={skipping} title="Skipping" />
+        <ExerciseCard img={lunges} title="Lunges" />
+        <ExerciseCard img={pullup} title="Pullup" />
+        <ExerciseCard img={weight} title="Weight Lifting" />
+        <ExerciseCard img={situps} title="Situps" />
+      </div>
+    </main>
   );
 }
 
