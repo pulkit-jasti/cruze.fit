6d707cadcffa2fd14e31d855ef14446e3737770b
pulkit-jasti <pulkit.jasti@gmail.com>
2022-03-30 01:07:37 -0700

    Final updates

--- backend/camera.py
+++ backend/camera.py
@@ -11,7 +11,7 @@ class VideoCamera(object):
         self.video = cv2.VideoCapture(0)
         self.detector = pm.poseDetector()
         self.cnt_sq=0
-        self.cnt_pu=0
+        self.f=0
         self.cnt_pu=0
         self.cnt_wt=0
         self.cnt_cr=0
@@ -26,9 +26,9 @@ class VideoCamera(object):
                             #print("assssssssssssssssssssss")
                             pTime = 0
                         
-                            self.cnt_sq = 0
+                            
 
-                            f=0
+                             
                         
                        # while self.cnt_sq < 100000000:
                             ret, img = self.video.read()
@@ -36,7 +36,7 @@ class VideoCamera(object):
                             lmlist = self.detector.getPosition(img,draw=False)
                             
                             # if u want all dots then put draw= true and comment out the cv2.circle part in the if part below
-                            
+                            #time.sleep(5)
                             if len(lmlist)!=0:
                                 cv2.circle(img,(lmlist[25][1],lmlist[25][2]),10,(0,0,255),cv2.FILLED)
                                 cv2.circle(img,(lmlist[23][1],lmlist[23][2]),10,(0,0,255),cv2.FILLED) 
@@ -45,18 +45,20 @@ class VideoCamera(object):
                                 y2 = lmlist[23][2]
                                 
                                 length = y2-y1
-                                if length>=0 and f==0:
-                                    f=1
-                                elif length<-50 and f==1:
-                                    f=0
+                                 
+                                if length>=0 and self.f==0:
+                                    self.f=1
+                                elif length<-50 and self.f==1:
+                                    self.f=0
                                     self.cnt_sq=self.cnt_sq+1       
+                                    print(self.cnt_sq)
 
                                 cTime = time.time()
                                 fps = 1/(cTime-pTime)
                                 pTime = cTime
                                 cv2.putText(img,"Total Number of Squats  "+str(int(self.cnt_sq)),(70,50),cv2.FONT_HERSHEY_DUPLEX,1,
                                 (60,100,255),3)
-                                cv2.putText(img,"Calories Burnt  "+str(int(self.cnt_sq)*0.32),(70,150),cv2.FONT_HERSHEY_DUPLEX,1,
+                                cv2.putText(img,"Calories Burnt  "+str(int(int(self.cnt_sq)*0.32)),(70,150),cv2.FONT_HERSHEY_DUPLEX,1,
                                 (60,100,255),3)
                                # img = cv2.resize(img, (600,600))                 # Resize image
                                 
@@ -66,9 +68,9 @@ class VideoCamera(object):
                             return jpeg.tobytes()
         
             elif ex=="push":
-                            self.cnt_sq = 0
+                            
 
-                            f=0
+                             
                         #time.sleep(5)
                        # while True and self.cnt_sq<10000:
                             ret, img = self.video.read()
@@ -83,10 +85,10 @@ class VideoCamera(object):
                                 y2 = lmlist[0][2]
                                 
                                 length = y2-y1
-                                if length>=0 and f==0:
-                                    f=1
-                                elif length<-50 and f==1:
-                                    f=0
+                                if length>=0 and self.f==0:
+                                    self.f=1
+                                elif length<-50 and self.f==1:
+                                    self.f=0
                                     self.cnt_sq=self.cnt_sq+1
 
 
@@ -95,7 +97,7 @@ class VideoCamera(object):
                             
                                 cv2.putText(img,"Pushup"+str(int(self.cnt_sq)),(70,250),cv2.FONT_HERSHEY_DUPLEX,2,
                                 (60,100,255),2)
-                                cv2.putText(img,"Burnt"+str(int(self.cnt_sq)*0.29),(70,350),cv2.FONT_HERSHEY_DUPLEX,2,
+                                cv2.putText(img,"Burnt"+str(int(int(self.cnt_sq)*0.29)),(70,350),cv2.FONT_HERSHEY_DUPLEX,2,
                                 (60,100,255),2)
                                # img = cv2.resize(img, (620,650))                    # Resize image
                                 
@@ -106,7 +108,7 @@ class VideoCamera(object):
                 ret, img = self.video.read()
                 img = self.detector.findPose(img)
                 lmlist = self.detector.getPosition(img,draw=False)
-                f=0
+                 
                 if len(lmlist)!=0:
                     cv2.circle(img,(lmlist[15][1],lmlist[15][2]),10,(0,0,255),cv2.FILLED)
                     cv2.circle(img,(lmlist[3][1],lmlist[3][2]),10,(0,0,255),cv2.FILLED) 
@@ -114,15 +116,15 @@ class VideoCamera(object):
                     y2 = lmlist[20][2]
                     
                     length = y2-y1
-                    if length>=0 and f==0:
-                        f=1
-                    elif length<0 and f==1:
-                        f=0
+                    if length>=0 and self.f==0:
+                        self.f=1
+                    elif length<0 and self.f==1:
+                        self.f=0
                         self.cnt_sq=self.cnt_sq+1
 
                     cv2.putText(img,"Total Number of Pullups  "+str(int(self.cnt_sq)),(70,250),cv2.FONT_HERSHEY_DUPLEX,3,
                     (60,100,255),3)
-                    cv2.putText(img,"Calories Burnt  "+str(int(self.cnt_sq)*1),(70,350),cv2.FONT_HERSHEY_DUPLEX,3,
+                    cv2.putText(img,"Calories Burnt  "+str(int(int(self.cnt_sq)*1)),(70,350),cv2.FONT_HERSHEY_DUPLEX,3,
                     (60,100,255),3)
                                         # Resize image
                 ret, jpeg = cv2.imencode('.jpg', img)
@@ -131,22 +133,22 @@ class VideoCamera(object):
                 ret, img = self.video.read()
                 img = self.detector.findPose(img)
                 lmlist = self.detector.getPosition(img,draw=False)
-                f=0
+                 
                 if len(lmlist)!=0:
                                 x1 = lmlist[0][1]
                                 x2 = lmlist[12][1]
                                 
                                 length = x1-x2
-                                if length>=0 and f==0:
-                                    f=1
-                                elif length<0 and f==1:
-                                    f=0
+                                if length>=0 and self.f==0:
+                                    self.f=1
+                                elif length<0 and self.f==1:
+                                    self.f=0
                                     self.cnt_sq=self.cnt_sq+1
 
  
                                 cv2.putText(img,"Total Number of Crunches  "+str(int(self.cnt_sq)),(70,50),cv2.FONT_HERSHEY_DUPLEX,1,
                                 (60,100,255),3)
-                                cv2.putText(img,"Calories Burnt  "+str(int(self.cnt_sq)*0.32),(70,150),cv2.FONT_HERSHEY_DUPLEX,1,
+                                cv2.putText(img,"Calories Burnt  "+str(int((self.cnt_sq)*0.32)),(70,150),cv2.FONT_HERSHEY_DUPLEX,1,
                                 (60,100,255),3)
                 ret, jpeg = cv2.imencode('.jpg', img)
                 return jpeg.tobytes()
@@ -154,7 +156,7 @@ class VideoCamera(object):
                 ret, img = self.video.read()
                 img = self.detector.findPose(img)
                 lmlist = self.detector.getPosition(img,draw=False)
-                f=0
+             
                 if len(lmlist)!=0:
                                 cv2.circle(img,(lmlist[17][1],lmlist[17][2]),20,(0,0,255),cv2.FILLED)
                                 cv2.circle(img,(lmlist[13][1],lmlist[13][2]),20,(0,255,0),cv2.FILLED) 
@@ -162,15 +164,15 @@ class VideoCamera(object):
                                 y2 = lmlist[13][2]
                                 
                                 length = y2-y1
-                                if length>=0 and f==0:
-                                    f=1
-                                elif length<0 and f==1:
-                                    f=0
+                                if length>=0 and self.f==0:
+                                    self.f=1
+                                elif length<0 and self.f==1:
+                                    self.f=0
                                     self.cnt_sq=self.cnt_sq+1
 
                                 cv2.putText(img,"Total Number of bicep curls  "+str(int(self.cnt_sq)),(70,50),cv2.FONT_HERSHEY_DUPLEX,1,
                                 (60,100,255),3)
-                                cv2.putText(img,"Calories Burnt  "+str(int(self.cnt_sq)*0.32),(70,150),cv2.FONT_HERSHEY_DUPLEX,1,
+                                cv2.putText(img,"Calories Burnt  "+str(int(int(self.cnt_sq)*0.32)),(70,150),cv2.FONT_HERSHEY_DUPLEX,1,
                                 (60,100,255),3)
                 ret, jpeg = cv2.imencode('.jpg', img)
                 return jpeg.tobytes()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import pushup from "./assets/pushup.png";
 import skipping from "./assets/skipping.png";
-import lunges from "./assets/lunges.png";
+import squats from "./assets/squats.png";
 import pullup from "./assets/pullup.png";
 import weight from "./assets/weight.png";
-import situps from "./assets/situps.png";
+import crunches from "./assets/crunches.png";
 import earth from "./assets/earth.png";
 
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
@@ -19,34 +19,96 @@ class App extends Component {
     super(props);
     this.state = {
       modal: false,
+      videoURL: earth,
+      exerciseTitle: "Null",
     };
-
-    this.toggle = this.toggle.bind(this);
   }
 
-  toggle() {
-    this.setState({
-      modal: !this.state.modal,
-    });
+  toggleModal(data) {
+    console.log("testtttt", data);
+
+    if (typeof data == "string") {
+      this.setState({
+        modal: !this.state.modal,
+      });
+    } else {
+      this.setState({
+        modal: !this.state.modal,
+        exerciseTitle: data.title,
+        videoURL: data.videoURL,
+      });
+    }
   }
 
   render() {
+    let exerciseData = [
+      {
+        title: "Crunches",
+        imgURL: crunches,
+        videoURL: "http://192.168.0.174:5000/cru",
+      },
+      {
+        title: "Squats",
+        imgURL: squats,
+        videoURL: "http://192.168.0.174:5000/squats",
+      },
+      {
+        title: "Pushups",
+        imgURL: pushup,
+        videoURL: "http://192.168.0.174:5000/push",
+      },
+      {
+        title: "Pullups",
+        imgURL: pullup,
+        videoURL: "http://192.168.0.174:5000/pull",
+      },
+      {
+        title: "Weight Lifting",
+        imgURL: weight,
+        videoURL: "http://192.168.0.174:5000/weight",
+      },
+      {
+        title: "Skipping",
+        imgURL: skipping,
+        videoURL: "http://192.168.0.174:5000/squats",
+      },
+    ];
+
     return (
       <main>
-        <Modal isOpen={this.state.modal} size="lg" toggle={this.toggle} className="primary-modal" centered>
-          <ModalHeader toggle={this.toggle}>Pushups</ModalHeader>
+        <Modal
+          isOpen={this.state.modal}
+          size="lg"
+          toggle={this.toggleModal.bind(this, "Null")}
+          className="primary-modal"
+          centered
+        >
+          <ModalHeader toggle={this.toggleModal.bind(this, "Null")}>{this.state.exerciseTitle}</ModalHeader>
           <ModalBody>
-            <img src={earth} alt="" />
+            <img src={this.state.videoURL} alt="" />
           </ModalBody>
         </Modal>
 
         <div className="card-container">
-          <ExerciseCard img={pushup} title="Pushups" onClick={this.toggle} />
-          <ExerciseCard img={skipping} title="Skipping" onClick={this.toggle} />
-          <ExerciseCard img={lunges} title="Lunges" onClick={this.toggle} />
-          <ExerciseCard img={pullup} title="Pullup" onClick={this.toggle} />
-          <ExerciseCard img={weight} title="Weight Lifting" onClick={this.toggle} />
-          <ExerciseCard img={situps} title="Situps" onClick={this.toggle} />
+          {/* <ExerciseCard img={pushup} title="Pushups" onClick={this.toggleModal} />
+          <ExerciseCard img={skipping} title="Skipping" onClick={this.toggleModal} />
+          <ExerciseCard img={lunges} title="Lunges" onClick={this.toggleModal} />
+          <ExerciseCard img={pullup} title="Pullup" onClick={this.toggleModal} />
+          <ExerciseCard img={weight} title="Weight Lifting" onClick={this.toggleModal} />
+          <ExerciseCard img={crunches} title="Situps" onClick={this.toggleModal} /> */}
+
+          {exerciseData.map((el, index) => {
+            return (
+              <div
+                className="exercise-card"
+                key={index}
+                onClick={this.toggleModal.bind(this, { title: el.title, videoURL: el.videoURL })}
+              >
+                <img src={el.imgURL} alt="" />
+                <h3>{el.title}</h3>
+              </div>
+            );
+          })}
         </div>
       </main>
     );
